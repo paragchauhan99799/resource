8f1b7d263e7675cb6111678cfe938b5bd38d5b60
GOODBOY <rajsuvariya@gmail.com>
2016-04-15 01:58:45 +0530

    trial4

--- models/book.js
+++ models/book.js
@@ -2,13 +2,15 @@ var mongoose = require('mongoose');
 
 var BookSchema = new mongoose.Schema({
 	ISBN : {
-		type :String,
-		required : true
+		type :String
 	},
 	accessionNumber : {
-		type : String,
-		unique : true
+		type : String
+	},
+	place : {
+		type : String
 	}		
-});
+})
+BookSchema.index({ISBN: 1, accessionNumber: 1}, {unique: true});
 
 module.exports = mongoose.model('book',BookSchema);
